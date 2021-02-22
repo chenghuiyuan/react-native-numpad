38774f0a2bf33498b62b3da13b0dc8bb3e19476f
chenghuiyuan <huiyuanhyhy@gmail.com>
2021-02-22 14:08:53 +0800

    update

--- App.js
+++ App.js
@@ -3,20 +3,12 @@ import { SafeAreaView } from 'react-native';
 import NumberPad, { Input, Display } from './index';
 import { Ionicons } from '@expo/vector-icons';
 
-import styles from './lib/styles';
-
 export default class App extends React.Component {
   render() {
     return (
       <NumberPad>
         <SafeAreaView>
           {[0, 1, 2].map((i) => (
-            <Display key={i} cursor value={101} />
-          ))}
-        </SafeAreaView>
-        <Text style={styles.displayText}>.</Text>
-        <SafeAreaView>
-          {[3, 4, 5].map((i) => (
             <Display key={i} cursor value={101} />
           ))}
         </SafeAreaView>
